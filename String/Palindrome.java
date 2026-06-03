7712f9fe214c78f470f873b01139fdcc83e2a89b
Prasanjeet Das <165247108+Prasenjeet-das@users.noreply.github.com>
2026-06-03 06:06:35 +0000

    sringpalindrome

---
+++ String/Palindrome.java
@@ -0,0 +1,14 @@
+public class Palindrome{
+    public static void main(String[] args){
+        String str = "malayalam";
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("The string is a palindrome.");
+        }else{
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
